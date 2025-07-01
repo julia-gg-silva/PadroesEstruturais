861b438593b5d598b3473cb8c0fe694b41164a9c
JULIA GABRIELI GARCIA DA SILVA <julia_gg_silva@estudante.sesisenai.org.br>
2025-06-30 21:17:04 -0300

    Respondendo as perguntas

--- src/atividadeAdapter/Main.java
+++ src/atividadeAdapter/Main.java
@@ -2,6 +2,14 @@ package atividadeAdapter;
 
 public class Main {
 
+//    Você precisa integrar um sistema de pagamento antigo com uma
+//    nova API moderna. As interfaces são diferentes. Como você faria
+//    para reaproveitar o código antigo sem alterar sua estrutura?
+
+//    Eu usei o padrão de design Adapter. Criei uma classe adaptadora (AdaptadorPagamento)
+//    que permite que a nova aplicação utilize o código do sistema de pagamento antigo sem
+//    precisar modificar a classe OldPagamento.
+
     public static void main(String[] args) {
     AdaptadorPagamento pagamento = new AdaptadorPagamento();
     pagamento.pagar(9.000);
--- src/atividadeDecorator/Main.java
+++ src/atividadeDecorator/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Usuários do sistema podem escolher gerar relatórios com ou sem
+//        cabeçalho, rodapé ou assinatura. Como permitir essa flexibilidade sem
+//        criar várias classes diferentes para cada combinação?
+
+//        Usei o padrão de design Decorator. Em vez de criar uma classe para
+//        cada combinação (por exemplo, com cabeçalho e rodapé ou com assinatura
+//        e rodapé), criei decoradores que podem ser "empilhados" dinamicamente,
+//            permitindo adicionar cabeçalho, rodapé e assinatura conforme a
+//        necessidade. Assim, posso combinar os elementos do relatório de forma
+//        flexível e sem criar muitas classes.
+
         Relatorio relatorio = new RelatorioSimples();
         relatorio = new DecoradorComCabecario(relatorio);
         relatorio = new DecoradorComRodape(relatorio);
